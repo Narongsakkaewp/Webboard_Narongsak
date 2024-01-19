f9568b70b6738aa1a5d20d739f360a47cdd9b96f
Narongsakkaewp <152575175+Narongsakkaewp@users.noreply.github.com>
2024-01-19 11:43:18 +0700

    Update Webboard
    Update Webboard with php function

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,16 +15,30 @@
     <form>
     หมวดหมู่: 
         <select name="category">
-        <option value="All">--ทั้งหมด--</option>
-        <option value="General">เรื่องทั่วไป</option>
-        <option value="Study">เรื่องเรียน</option>
+            <option value="All">--ทั้งหมด--</option>
+            <option value="General">เรื่องทั่วไป</option>
+            <option value="Study">เรื่องเรียน</option>
         </select>
-        <a href="login.html" style="float: right;">เข้าสู่ระบบ</a>
+        <?php
+            if(!isset($_SESSION['id'])){
+                echo "<a href=login.php style='float: right';> เข้าสู่ระบบ </a>";
+            }else{
+                echo "<div style='float: right';>
+                    ผู้ใช้งานระบบ : $_SESSION[username]&nbsp;&nbsp;
+                    <a href=logout.php>ออกจากระบบ</a>
+                </div>";
+                echo "<br><a href=newpost.php>สร้างกระทู้ใหม่</a>";
+            }
+        ?>
     </form>
     <ul>
         <?php
             for($i = 1; $i<=10; $i++){
-                echo "<li><a href=post.php?id=$i>กระทู้ที่ $i </a></li>";
+                echo "<li><a href=post.php?id=$i>กระทู้ที่$i</a>";
+                if(isset($_SESSION['id'])&&$_SESSION['role']=='a'){
+                    echo"&nbsp;&nbsp;<a href=delete.php?id=$i>ลบ</a>";
+                }
+                echo"</li>";
             }
         ?>
     </ul>
