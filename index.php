aa3a1bcf53319b37e49e5bdd8f9e3fe8c7907125
Narongsakkaewp <152575175+Narongsakkaewp@users.noreply.github.com>
2024-02-09 12:13:22 +0700

    Update index.php
    Update index.php Navbar

--- index.php
+++ index.php
@@ -3,24 +3,55 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>Webboard</title>
-    
 </head>
+
 <body>
-    <h1 style="text-align: center;">Narongsak's Webboard</h1>
-    <hr>
-    <form>
-    หมวดหมู่: 
-        <select name="category">
-            <option value="All">--ทั้งหมด--</option>
-            <option value="General">เรื่องทั่วไป</option>
-            <option value="Study">เรื่องเรียน</option>
-        </select>
-        <?php
+    <div class="container-lg">
+        <h1 style="text-align: center;" class="mt-3">Narongsak's Webboard</h1>
+        <nav class="navbar navbar-expand-lg" style="background-color: #d3d3d3;">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="Index.php"> <i class="bi bi-house-fill"></i> Home</a>
+                <ul class="navbar-nav">
+                    <?php if(!isset($_SESSION['id'])){?>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="login.php"><i
+                            class="bi bi-pencil-square"></i>เข้าสู่ระบบ</a>
+                    </li>
+                    <?php }else{ ?>
+                    <li class="nav-item dropdown">
+                        <a class="btn btn-outline-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            <i class="bi bi-person-lines-fill"></i> <?php echo $_SESSION['username']; ?>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="logout.php"><i class="bi bi-box-arrow-left"></i> ออกจากระบบ</a></li>
+                        </ul>
+                    </li>
+
+                    <?php } ?>
+                </ul>
+            </div>
+        </nav>
+        <form>
+            หมวดหมู่:
+            <select name="category">
+                <option value="All">--ทั้งหมด--</option>
+                <option value="General">เรื่องทั่วไป</option>
+                <option value="Study">เรื่องเรียน</option>
+            </select>
+            <?php
             if(!isset($_SESSION['id'])){
                 echo "<a href=login.php style='float: right';> เข้าสู่ระบบ </a>";
             }else{
@@ -31,9 +62,9 @@ session_start();
                 echo "<br><a href=newpost.php>สร้างกระทู้ใหม่</a>";
             }
         ?>
-    </form>
-    <ul>
-        <?php
+        </form>
+        <ul>
+            <?php
             for($i = 1; $i<=10; $i++){
                 echo "<li><a href=post.php?id=$i>กระทู้ที่$i</a>";
                 if(isset($_SESSION['id'])&&$_SESSION['role']=='a'){
@@ -42,6 +73,8 @@ session_start();
                 echo"</li>";
             }
         ?>
-    </ul>
+        </ul>
+    </div>
 </body>
+
 </html>
