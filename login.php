b4a3df8b41afa3b4137f1f7e54fd3ff7b14d13f6
Narongsakkaewp <152575175+Narongsakkaewp@users.noreply.github.com>
2024-02-23 10:53:22 +0700

    Work 7 finished (Bootstrap2)
    Work 7 finished (Bootstrap2)

--- login.php
+++ login.php
@@ -13,31 +13,52 @@ if(isset($_SESSION['id'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 
 <body>
-    <h1 style="text-align: center;">Login Webboard</h1>
-    <hr>
-    <form action="verify.php" method="post">
-        <table style="border: 2px solid black; width: 45%;" align="center">
-            <tr>
-                <td colspan="2" style="background-color: #6CD2FE;">เข้าสู่ระบบ</td>
-            </tr>
-            <tr>
-                <td>Login</td> <td> <input type="text" name="login" size="50" required> </td>
-            </tr>
-            <tr>
-                <td>Password</td> <td> <input type="password" name="pwd" size="50" required> </td>
-            </tr>
-            <tr>
-                <td colspan="2" align="center"> <input type="submit" value="Login"> </td>
-            </tr>
-        </table>
-        <br>
-        <div style="text-align: center;">
+    <div class="container-lg">
+    <h1 style="text-align: center;" class="mt-3">Login Webboard</h1>
+    <?php include "nav.php"?>
+    <div class="row">
+        <div class="col-lg-4 col-md-3 col-sm-2 col-1"></div>
+        <div class="col-lg-4 col-md-6 col-sm-8 col-10">
+            <?php
+            if(isset($_SESSION['error']))
+            echo "<div class='alert alert-danger mt-4'>ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง</div>";
+            unset($_SESSION['error']);
+            ?>
+            <div class="card mt-4" >
+                <div class="card-header">เข้าสู่ระบบ</div>
+                <div class="card-body">
+                    <form action="verify.php" method="post">
+                        <div class="form-group">
+                            <label for="user" class="form-label">Login:</label>
+                            <input type="text" name="login" id="user" class="form-control" required>
+                        </div>
+                        <div class="form-group mt-2">
+                            <label for="pwd" class="form-label">Password:</label>
+                            <input type="password" name="pwd" id="pwd" class="form-control" required>
+                        </div>
+                        <div class="mt-3 justify-content-center d-flex">
+                            <input type="submit" value="Login" class="btn btn-secondary btn-sm me-2">
+                            <input type="submit" value="Reset" class="btn btn-danger btn-sm">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-3 col-sm-2 col-1"></div>
+    </div>
+    <div style="text-align: center;" class="mt-2">
             ถ้ายังไม่ได้เป็นสมาชิก <a href="register.php">กรุณาสมัครสมาชิก</a>
         </div>
-    </form>
+    </div>
 </body>
 
 </html>
--- verify.php
+++ verify.php
@@ -14,8 +14,7 @@ if(isset($_SESSION['id'])){
     <title>Veriify</title>
 </head>
 <body>
-    <h1 style="text-align: center;">Narongsak's Webboard</h1>
-    <hr>
+
     <div style="text-align: center;">
     <?php
     $login= $_POST["login"];
@@ -24,15 +23,22 @@ if(isset($_SESSION['id'])){
         $_SESSION['username'] = 'admin';
         $_SESSION['role'] = 'a';
         $_SESSION['id'] =session_id();
-        echo "ยินดีต้อนรับคุณ ADMIN <br>";
+        header("location:index.php");
+        die();
+        //echo "ยินดีต้อนรับคุณ ADMIN <br>";
     }
     elseif($login=="member" && $pwd=="mem1234"){
         $_SESSION['username'] = 'member';
         $_SESSION['role'] = 'm';
         $_SESSION['id'] =session_id();
-        echo "ยินดีต้อนรับคุณ MEMBER <br>";
+        header("location:index.php");
+        die();
+        //echo "ยินดีต้อนรับคุณ MEMBER <br>";
     }else{
-        echo "ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง<br>";
+        $_SESSION['error']='error';
+        header("location:login.php");
+        die();
+        //echo "ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง<br>";
     }
 
     /*echo "เข้าสู่ระบบด้วย <br>";
@@ -40,7 +46,7 @@ if(isset($_SESSION['id'])){
     echo "Password = $_POST[pwd]<br>"; //สามารถใช้แบบนี้ได้ โดยไม่ต้องเปิดแท็ก php ประจำ
     */
     ?>
-    <a href="index.php">กลับไปหน้าหลัก</a>
+    <!--<a href="index.php">กลับไปหน้าหลัก</a>-->
     </div>
 </body>
 </html>
